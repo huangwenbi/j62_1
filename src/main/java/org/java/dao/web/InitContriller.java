53126209e54238e1edda87a7f5f2ca924faf6f29
huangwenbi <279859hnjzq>
2019-09-02 15:06:03 +0800

    11

---
+++ src/main/java/org/java/dao/web/InitContriller.java
@@ -0,0 +1,16 @@
+package org.java.dao.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class InitContriller {
+
+    @RequestMapping("init/{jake}")
+   public String init(@PathVariable("jake") String jake, Model model){
+       model.addAttribute("user",jake);
+       return "/ok";
+   }
+}
---
+++ src/main/webapp/init.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2019/8/30
+  Time: 16:28
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+<A href="init/jake.do">进入</A>
+
+
+</body>
+</html>
